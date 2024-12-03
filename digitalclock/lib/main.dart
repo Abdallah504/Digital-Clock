f25e4f03956de3ce95b1916faab8b4ebf1718338
Abdallah Hossam <53866723+Abdallah504@users.noreply.github.com>
2024-12-03 13:48:42 +0200

    code

---
+++ digitalclock/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:digitalclock/screens/clock-digi.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      theme: Theme.of(context).copyWith(
+        textTheme: GoogleFonts.pressStart2pTextTheme(),
+        scaffoldBackgroundColor: Colors.black12,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const ClockScreen(),
+    );
+  }
+}
+
