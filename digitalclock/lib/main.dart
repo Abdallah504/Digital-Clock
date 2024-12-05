1cbf753e7559761ca9c09961e2e527a912856f3c
Abdallah Hossam <53866723+Abdallah504@users.noreply.github.com>
2024-12-05 10:45:28 +0200

    clock

--- digitalclock/lib/main.dart
+++ digitalclock/lib/main.dart
@@ -1,25 +1,101 @@
-import 'package:digitalclock/screens/clock-digi.dart';
+// import 'package:digitalclock/screens/clock-digi.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowMaterialGrid: false,
+//       theme: Theme.of(context).copyWith(
+//         textTheme: GoogleFonts.pressStart2pTextTheme(),
+//         scaffoldBackgroundColor: Colors.black,
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: const ClockScreen(),
+//     );
+//   }
+// }
+//
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'dart:async'; // For Timer
+import 'package:intl/intl.dart'; // Optional for time formatting
+
 void main() {
-  runApp(const MyApp());
+  runApp(DigitalClockApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class DigitalClockApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowMaterialGrid: false,
-      theme: Theme.of(context).copyWith(
-        textTheme: GoogleFonts.pressStart2pTextTheme(),
-        scaffoldBackgroundColor: Colors.black,
-      ),
       debugShowCheckedModeBanner: false,
-      home: const ClockScreen(),
+      title: 'Digital Clock',
+      theme: ThemeData.dark(),
+      home: DigitalClockScreen(),
     );
   }
 }
 
+class DigitalClockScreen extends StatefulWidget {
+  @override
+  _DigitalClockScreenState createState() => _DigitalClockScreenState();
+}
+
+class _DigitalClockScreenState extends State<DigitalClockScreen> {
+  String _time = ""; // Variable to store the current time
+  String _date = ""; // Variable to store the current date
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime(); // Initial time setup
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _updateTime());
+  }
+
+  void _updateTime() {
+    final now = DateTime.now();
+    setState(() {
+      _time = DateFormat('HH:mm:ss').format(now); // Format: 24-hour time
+      _date = DateFormat('EEEE, MMM d, y').format(now); // Format: Full date
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digital Clock'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _time,
+              style: TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              _date,
+              style: TextStyle(
+                fontSize: 24,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
